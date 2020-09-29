74057413db9a2bd53974942b6b6628f76d62ef48
dineo789 <dineo.thato06@gmail.com>
2020-09-29 23:26:53 +0200

    Add explicit intent to start Phone Activity Add generated local configurables

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -21,13 +21,8 @@ public class InfoActivity extends AppCompatActivity {
         Picasso.with(this).
                 load(uri).
                 into(candyStoreImageView);
-
-
     }
 
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
     public void createMapIntent(View view) {
         Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
@@ -37,7 +32,10 @@ public class InfoActivity extends AppCompatActivity {
         }
     }
 
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
+    public void createPhoneIntent(View view) {
+        Uri uri = Uri.parse("tel:0123456789");
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(uri);
+        startActivity(phoneIntent);
+    }
 }
